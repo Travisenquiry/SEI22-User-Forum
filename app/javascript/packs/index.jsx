85ecd0b6164b10633bee17a2ae55102ddb00498a
travisenquiry <travisenquiry@gmail.com>
2020-06-09 17:47:48 +0800

    prepared multi routing into one page and added validation for invalid pages

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,21 +6,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const Index = props => (
+  <div>Index here</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Index />,
     document.body.appendChild(document.createElement('div')),
   )
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'index' => 'onepage#index'
+  root 'onepage#index'
   resources :communities
   devise_for :users, :controllers => { registrations: 'registrations' }
+  match ':not_found' => redirect('/'), :via => [:get],
+  :constraints => { :not_found => /.*/ }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
