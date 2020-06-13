129c76918d7d5e8ed75a39bc6d9778a9aa67cca6
travisenquiry <travisenquiry@gmail.com>
2020-06-13 15:50:29 +0800

    added new route for community with proper redirect

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -29,7 +29,7 @@ class CommunitiesController < ApplicationController
 
     respond_to do |format|
       if @community.save
-        format.html { redirect_to @community, notice: 'Community was successfully created.' }
+        format.html { redirect_to '/', notice: 'Community was successfully created.' }
         format.json { render :show, status: :created, location: @community }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'community/:id/topics/new' => 'topics#new'
   get 'community/:id/topics/:topic_id' => 'onepage#topic'
   get 'community/:id' => 'onepage#community'
+  get 'community/new' => 'communities#new'
   resources :communities
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'onepage#index'
