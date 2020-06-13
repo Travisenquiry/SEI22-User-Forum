86a9a1202725690173b56391e392ca5d2e2e69e2
travisenquiry <travisenquiry@gmail.com>
2020-06-13 20:36:40 +0800

    fixed comment controller to look identical to others

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,8 +32,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
-
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
